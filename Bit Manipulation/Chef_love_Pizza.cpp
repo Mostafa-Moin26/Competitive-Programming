844a75e0a5e833312085b643c2aa3dd709bfa7e3
Mostafa Al Moin <sa479374@gmail.com>
2024-07-13 10:31:18 +0600

    solution

---
+++ Bit Manipulation/Chef_love_Pizza.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define all(x) (x).begin(), (x).end()
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+bool comp(int a, int b) {return a > b;}
+
+int main() {
+	Mostafa
+	// https://www.codechef.com/problems/CHEFIZZA
+
+	w(x) {
+		int x; cin >> x;
+
+		int lsb = 0;
+		int tmp = x;
+
+		while (tmp != 0) {
+			lsb++;
+			tmp = tmp >> 1;
+		}
+		cout << x - ((1 << lsb) - x) << endl;
+	}
+	return 0;
+}
